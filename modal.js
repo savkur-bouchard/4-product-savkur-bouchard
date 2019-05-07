ad7e7f61d10bbe963330d62614cb9caf9dea36ec
nsavkur2022 <50105589+nsavkur2022@users.noreply.github.com>
2019-05-07 08:22:57 -0400

    Add files via upload

--- modal.js
+++ modal.js
@@ -10,6 +10,7 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
 // When the user clicks on <span> (x), close the modal
